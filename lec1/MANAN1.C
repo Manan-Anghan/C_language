6d5a5bde791a24ae8821d9a034d621a49e2b68a8
Manan-Anghan <126051337+Manan-Anghan@users.noreply.github.com>
2023-03-09 02:09:18 -0800

    Add files via upload

---
+++ lec1/MANAN1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+   clrscr();
+   printf("    m   \n ");
+   printf(" a    a\n");
+   printf("n        n\n");
+   printf("  a    a\n");
+   printf("    n\n");
+   getch();
+}
