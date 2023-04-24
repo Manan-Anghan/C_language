eb4a7606fab4cc368c928c60b5da5c3b9f829bf4
Manan-Anghan <126051337+Manan-Anghan@users.noreply.github.com>
2023-04-24 14:01:20 +0530

    Update p9_1.c

--- project 9/p9_1.c
+++ project 9/p9_1.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
 
-enum week{
-	mon=1,tue,wed,thur,fri,sat,sun
+struct dis {
+   int	inch,feet,inch1,feet1,inch2,feet2;
 };
-
 main()
 {
-	int day;
-	day=tue;
-	printf("the day of week is %d:",day);
+	struct dis d;
+	
+	printf("Enter the first distance : ");
+	printf("\nEnter inch : ");
+	scanf("%d",&d.inch1);
+	printf("Enter feet : ");
+	scanf("%d",&d.feet1);
+	
+	printf("\nEnter the second distance : ");
+	printf("\nEnter inch : ");
+	scanf("%d",&d.inch2);
+	printf("Enter feet : ");
+	scanf("%d",&d.feet2);
+	
+	d.inch=d.inch1+d.inch2;
+	d.feet=d.feet1+d.feet2;
+	
+	while(d.inch>=12)
+	{
+		d.feet++;
+		d.inch-=12;	
+	}
+	printf("\nThe total distance is %d inch and %d feet.",d.inch,d.feet);
+
 }
